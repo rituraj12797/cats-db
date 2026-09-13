304c4e90ff4359b98ef3e0f723d6f5e75601c9da
rituraj12797 <alchemyrecloak@gmail.com>
2026-09-13 20:58:54 +0000

    Concurrent Skiplist: implemented seek method

--- components/lockfree_skiplist/lockfree_skiplist.go
+++ components/lockfree_skiplist/lockfree_skiplist.go
@@ -13,10 +13,54 @@ type CompositeKey struct {
 	versionNumber int64
 }
 
+func (K *CompositeKey) LessThan(other *CompositeKey) bool {
+	if K.key < other.key {
+		return true
+	}
+	if K.key == other.key && K.versionNumber > other.versionNumber {
+		return true
+	}
+	return false
+}
+
+func (K *CompositeKey) LessThanEqualTo(other *CompositeKey) bool {
+	if K.key < other.key {
+		return true
+	}
+	if K.key == other.key && K.versionNumber >= other.versionNumber {
+		return true
+	}
+	return false
+}
+
+func (K *CompositeKey) MoreThan(other *CompositeKey) bool {
+	if K.key > other.key {
+		return true
+	}
+	if K.key == other.key && K.versionNumber < other.versionNumber {
+		return true
+	}
+	return false
+}
+
+func (K *CompositeKey) MoreThanEqualTo(other *CompositeKey) bool {
+	if K.key > other.key {
+		return true
+	}
+	if K.key == other.key && K.versionNumber <= other.versionNumber {
+		return true
+	}
+	return false
+}
+
+func (K *CompositeKey) EqualTo(other *CompositeKey) bool {
+	return K.key == other.key && K.versionNumber == other.versionNumber
+}
+
 type SkipListNode struct {
 	nodeKey      CompositeKey
 	nodeValue    int64
-	pointerArray []*SkipListNode
+	pointerArray []atomic.Pointer[SkipListNode] // array of atomic pointers
 	tombstone    bool
 }
 
@@ -29,12 +73,12 @@ type SkipList struct {
 func NewSkipList() *SkipList {
 
 	start := &SkipListNode{
-		pointerArray: make([]*SkipListNode, 0),
+		pointerArray: make([]atomic.Pointer[SkipListNode], 0),
 	}
 
 	end := &SkipListNode{}
 
-	start.pointerArray = append(start.pointerArray, end)
+	start.pointerArray[0].Store(end)
 
 	var s1 atomic.Int64
 	s1.Store(1)
@@ -56,19 +100,46 @@ func randomLevel() int {
 	return level
 }
 
-func (S *SkipList) Seek(key int64, readTransactionNumber atomic.Int64) *SkipListNode {
+func (s *SkipList) Seek(key int64, readTransactionNumber int64) *SkipListNode {
 	// receives a key along with a read transaction number assigned to this request
 	// returns a pointer to a node with the same key and latest possible versionNumber, else nil
+
+	currentPointer := s.start // reads start, since start's pointer never changes we need not to use atomic here
+
+	compositeTargetKey := CompositeKey{
+		key:           key,
+		versionNumber: readTransactionNumber,
+	}
+
+	for level := len(s.start.pointerArray) - 1; level >= 0; level-- {
+		nextPointer := currentPointer.pointerArray[level].Load() // atomically load the forward pointer of current node for this level
+
+		for nextPointer != s.end && (nextPointer.nodeKey.LessThan(&compositeTargetKey)) {
+			currentPointer = nextPointer
+			nextPointer = currentPointer.pointerArray[level].Load()
+		}
+	}
+
+	// since we used lessThan we found the largest node which is less than compositeTargetKey, so the possible candidate is it's first child on level 0
+	candidate := currentPointer.pointerArray[0].Load()
+
+	if candidate != s.end && candidate.nodeKey.key == key {
+		if !candidate.tombstone {
+			return candidate
+		}
+	}
+
+	return nil
 }
 
-func (S *SkipList) Insert(key int64, value int64, writeTransactionNumber atomic.Int64, delete bool) *SkipListNode {
+func (S *SkipList) Insert(key int64, value int64, writeTransactionNumber int64, delete bool) *SkipListNode {
 	// receives a key, value and a write transaction number assigned to this insert request
 	// receives a delete boolean, to decide weather to flag a node as tombstone or not
 	// returns a pointer to the inserted node, else nil
 	// this writeTransactionNumber is calculated as (SkipList.load(currentVersion) + 1)
 }
 
-func (S *SkipList) Update(key int64, value int64, updateTransactionNumber atomic.Int64) *SkipListNode {
+func (S *SkipList) Update(key int64, value int64, updateTransactionNumber int64) *SkipListNode {
 	// receives a key, value and an update transaction number assigned to this update request
 	// returns a pointer to the inserted node, else nil
 	// since we want this skip list to be append only, we nevere update inplace instead for updating
@@ -77,7 +148,7 @@ func (S *SkipList) Update(key int64, value int64, updateTransactionNumber atomic
 	return S.Insert(key, value, updateTransactionNumber, false)
 }
 
-func (S *SkipList) Delete(key int64, deleteTransactionNumber atomic.Int64) bool {
+func (S *SkipList) Delete(key int64, deleteTransactionNumber int64) bool {
 	// receives a key and a delete transaction number assigned to this delete request
 	// returns boolen based on succes of the delete operation
 	// since this skip list is append only, we never actually delete the entry but add a newer version of the same key with a tombstone indicating it's deleted status
